0e8378735daae52f532cb8f480cde4625eed2186
Nessaj44 <johnedmoncoc@gmail.com>
2023-02-19 12:57:39 +0800

    Add components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+export default function Cart() {
+  return (
+    <>
+      <Navbar />
+      
+      <h1>Cart</h1>
+    </>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Shopping cart</h1>
+
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+
+        <li>
+          <a href="">Shop</a>
+        </li>
+
+        <li>
+          <a href="">Cart</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
