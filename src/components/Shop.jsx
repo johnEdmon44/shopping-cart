a2fc1199e8b0c3ba1a03753fa245fd480918b09e
Nessaj44 <johnedmoncoc@gmail.com>
2023-02-20 16:30:45 +0800

    Add shop nav and shop items

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,9 +2,74 @@ import Navbar from "./Navbar";
 
 export default function Shop() {
   return (
-    <>
+    <section className="shop">
       <Navbar />
-      <h1>Shop</h1>
-    </>
+
+      <div className="shop-container">
+        <nav className="shop-nav">
+          <h1>SHOPS / ALLL</h1>
+
+
+          <ul>
+            <li>ALL</li>
+            <li>PHONES</li>
+            <li>TABLETS</li>
+            <li>LAPTOPS</li>
+          </ul>
+        </nav> 
+
+
+        <div className="items-container">
+          <div className="item">
+            <div className="item-img">
+              <p>img</p>
+            </div>
+
+            <p>Xiaomi redmi note 5</p>
+            <p>$499</p>
+          </div>
+
+
+          <div className="item">
+            <div className="item-img">
+              <p>img</p>
+            </div>
+
+            <p>Xiaomi redmi note 5</p>
+            <p>$499</p>
+          </div>
+
+
+          <div className="item">
+            <div className="item-img">
+              <p>img</p>
+            </div>
+
+            <p>Xiaomi redmi note 5</p>
+            <p>$499</p>
+          </div>
+
+
+          <div className="item">
+            <div className="item-img">
+              <p>img</p>
+            </div>
+
+            <p>Xiaomi redmi note 5</p>
+            <p>$499</p>
+          </div>
+
+
+          <div className="item">
+            <div className="item-img">
+              <p>img</p>
+            </div>
+
+            <p>Xiaomi redmi note 5</p>
+            <p>$499</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
